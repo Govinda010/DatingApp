1ac352885bd058fcdf481002d3e22ab65aa74ca0
Govinda Shrestha <gsth010@gmail.com>
2022-12-04 20:49:22 +0100

    Account Controller added with HMACSHA512

--- API/Enitites/AppUser.cs
+++ API/Enitites/AppUser.cs
@@ -9,4 +9,6 @@ public class AppUser
 {
     public int Id { get; set; }
     public string UserName { get; set; }
+    public byte[] PasswordHash { get; set; }
+    public byte[] PassowrdSlat { get; set; }
 }
