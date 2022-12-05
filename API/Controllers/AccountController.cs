7fbca63bdcd52ebf28f2c60502dfb1a320559d28
Govinda Shrestha <gsth010@gmail.com>
2022-12-05 22:22:00 +0100

    Validation added

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -2,8 +2,10 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Enitites;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
@@ -17,19 +19,24 @@ namespace API.Controllers
         }
 
         [HttpPost("register")] //Post: api/account/register
-        public async Task<ActionResult<AppUser>>Register(string username, string password)
+        public async Task<ActionResult<AppUser>>Register(RegisterDto registerDto)
         {
+            if (await UserExists(registerDto.Username)) return BadRequest("Username is taken");
             using var hmac = new HMACSHA512();
             var user = new AppUser()
             {
-                UserName = username,
-                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+                UserName = registerDto.Username.ToLower(),
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
                 PassowrdSlat = hmac.Key
 
             };
             _context.Users.Add(user);
             await _context.SaveChangesAsync();
             return user;
+        }
+        private async Task<bool>UserExists(string username)
+        {
+            return await _context.Users.AnyAsync(x=>x.UserName== username.ToLower());
         }
         
     }
