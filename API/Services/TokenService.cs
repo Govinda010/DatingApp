b5365d5dc721f70869c0ff726fadaee09128abe8
Govinda Shrestha <gsth010@gmail.com>
2022-12-11 01:08:49 +0100

    JWT token implemented

--- API/Services/TokenService.cs
+++ API/Services/TokenService.cs
@@ -1,17 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.IdentityModel.Tokens;
 using API.Enitites;
 using API.Interface;
+using System.Text;
+using System.Security.Claims;
+using System.IdentityModel.Tokens.Jwt;
 
 namespace API.Services
 {
     public class TokenService : ITokenService
     {
+        private readonly SymmetricSecurityKey _key;
+        public TokenService(IConfiguration config)
+        {
+            _key = new SymmetricSecurityKey(Encoding.UTF8
+            .GetBytes(config["TokenKey"]));
+        }
         public string CreateToken(AppUser user)
         {
-            throw new NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.Name,user.UserName)
+            };
+
+            var creds = new SigningCredentials(_key,
+            SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+             var tokenHandler = new JwtSecurityTokenHandler();
+             var token = tokenHandler.CreateToken(tokenDescriptor);
+             return tokenHandler.WriteToken(token);
         }
     }
 }
