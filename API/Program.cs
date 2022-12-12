dfc6daf51eb75896f1c911fe97ca8362d524b17c
Govinda Shrestha <gsth010@gmail.com>
2022-12-12 23:35:06 +0100

    Added extension methods

--- API/Program.cs
+++ API/Program.cs
@@ -5,30 +5,16 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using System.Text;
+using API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddCors();
-builder.Services.AddDbContext<DataContext>(opt =>
-{
-    opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
-});
-builder.Services.AddScoped<ITokenService, TokenService>();
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-  .AddJwtBearer(options=>
-  {
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuerSigningKey = true,
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8
-    .GetBytes(builder.Configuration["TokenKey"])),
-    ValidateIssuer = false,
-    ValidateAudience =false
-    };
-  });
+builder.Services.AddApplicationServices(builder.Configuration);
+builder.Services.AddIdentityService(builder.Configuration);
+
 var app = builder.Build();
 app.UseCors(builder=>builder.AllowAnyHeader()
             .AllowAnyMethod().WithOrigins("https://localhost:4200"));
