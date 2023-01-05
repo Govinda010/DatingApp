0631710deb38e640350ce1d158d11bd2686dfbde
Govinda Shrestha <gsth010@gmail.com>
2023-01-05 03:37:20 +0100

    guard component added

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
 import { AccountService } from '../_services/account.service';
 
 @Component({
@@ -11,7 +12,8 @@ export class RegisterComponent implements OnInit{
   users:any;
   @Output() cancelRegister = new EventEmitter();
 
-  constructor(private accountService: AccountService){}
+  constructor(private accountService: AccountService,
+     private toastr: ToastrService){}
 
   ngOnInit(): void {
     
@@ -21,7 +23,7 @@ export class RegisterComponent implements OnInit{
       next:()=>{
         this.cancel();
       },
-      error:error=>console.log(error)     
+      error:error=>this.toastr.error(error.error)     
     })
   }
   cancel(){
