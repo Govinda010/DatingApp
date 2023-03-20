66e8072fb4c7a8f034dd559e5d8cd2c16f9af0e0
Govinda Shrestha <gsth010@gmail.com>
2023-03-20 22:04:52 +0100

    API folder created

--- API/Interface/ITokenService.cs
+++
@@ -1,9 +0,0 @@
-using API.Enitites;
-
-namespace API.Interface
-{
-    public interface ITokenService
-    {
-        string CreateToken(AppUser user);
-    }
-}
--- API/Program.cs
+++ API/Program.cs
@@ -1,28 +1,25 @@
-using API.Data;
-using API.Interface;
-using API.Services;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
-using API.Extensions;
-using API.Middleware;
-
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-builder.Services.AddApplicationServices(builder.Configuration);
-builder.Services.AddIdentityService(builder.Configuration);
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.UseMiddleware<ExceptionMiddleware>();
-app.UseCors(builder=>builder.AllowAnyHeader()
-            .AllowAnyMethod().WithOrigins("https://localhost:4200"));
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-app.UseAuthentication();
-app.UseAuthorization();
+//app.UseHttpsRedirection();
+
+//app.UseAuthorization();
+
 app.MapControllers();
+
 app.Run();
