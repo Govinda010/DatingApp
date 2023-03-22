3af1514f2c732fc7c825dd251362c901dd42bb23
Govinda Shrestha <gsth010@gmail.com>
2023-03-22 01:11:40 +0100

    DataContext class created

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+namespace API.Entities
+{   
+    /// <summary>
+    /// Model Class for the AppUser having filed
+    /// Id and Username
+    /// </summary>
+    public class AppUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+
+    }
+}
