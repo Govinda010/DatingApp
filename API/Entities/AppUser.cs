4f620ad2c208d068b7babc45dab3390b40097f96
Govinda Shrestha <gsth010@gmail.com>
2023-03-23 01:22:37 +0100

    Http Api to get user and users are complete

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -7,7 +7,7 @@ namespace API.Entities
     public class AppUser
     {
         public int Id { get; set; }
-        public string Username { get; set; }
+        public string UserName { get; set; }
 
 
     }
