3326fa6ba6be2170bea2ccb0ae74ed897c69c66c
Govinda Shrestha <gsth010@gmail.com>
2023-03-28 23:22:03 +0200

    SSL added for https

--- API/Program.cs
+++ API/Program.cs
@@ -26,7 +26,7 @@ if (app.Environment.IsDevelopment())
 }
 
 
-app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
 
 //app.UseHttpsRedirection();
 
