8071bc099684ba04067b90793e2b73f5ee662b68
Govinda Shrestha <gsth010@gmail.com>
2023-04-24 02:18:42 +0200

    Extra field added for Appuser

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,3 +1,5 @@
+using API.Extensions;
+
 namespace API.Entities
 {   
     /// <summary>
@@ -11,6 +13,22 @@ namespace API.Entities
         
         public byte[] PasswordHash { get; set; }
         public byte[] PasswordSalt { get; set; }
+        public DateOnly DateOfBirth { get; set; } 
+        public DateTime Created { get; set; }= DateTime.UtcNow;
+        public string KnownAs { get; set; }
+        public DateTime LastActive { get; set; }= DateTime.UtcNow;
+        public string Gender { get; set; }
+        public string Introdcution { get; set; }
+        public string LookingFor { get; set; }
+        public string Interests { get; set; }
+        public string City { get; set; }
+        public string Country { get; set; }
+        public List<Photo> Photos { get; set; } = new();
+
+        public  int GetAge()
+        {
+            return DateOfBirth.CalculateAge();
+        }
 
 
     }
