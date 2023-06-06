f81d161b0dbd0237a4a5f67eae1df7bf30e370e3
Govinda010 <gsth010@gmail.com>
2023-06-06 23:03:41 +0200

    registration completed in client side

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -2,6 +2,7 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Validators } from '@angular/forms';
+import { Route, Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -10,13 +11,13 @@ import { AbstractControl, FormBuilder, FormControl, FormGroup, ValidatorFn, Vali
 })
 export class RegisterComponent implements OnInit {
   @Output() cancelRegister = new EventEmitter();
-  model:any = {};
   registerForm: FormGroup = new FormGroup({});
-  maxDate:Date = new Date()
+  maxDate:Date = new Date();
+  validationErrors:string[]|undefined;
 
 
   constructor(private accountService:AccountService,private toastr:ToastrService,
-      private fb:FormBuilder){}
+      private fb:FormBuilder,private router:Router){}
 
   ngOnInit(): void {
     this.initializeForm();
@@ -49,17 +50,25 @@ export class RegisterComponent implements OnInit {
   }
 
   register(){
-    console.log(this.registerForm.value);
-    // this.accountService.register(this.model).subscribe({
-    //   next:()=>{
-    //     this.cancel();
-    //   },
-    //   error:error=>{
-    //     this.toastr.error(error.error);
-    //     console.log(error);
-    //   }
+    const dob = this.getDateOnly(this.registerForm.controls['dateOfBirth'].value)
+    const values = {...this.registerForm.value, dateOfBirth:dob}    
+    this.accountService.register(values).subscribe({
+      next:()=>{
+        this.router.navigateByUrl('/members');
+      },
+      error:error=>{
+        this.validationErrors = error;
+        console.log("errors",error)
+      }
 
-    // })
+    })
+  }
+  private getDateOnly(dob:string|undefined)
+  {
+    if(!dob) return;
+    let thedob = new Date(dob);
+    return new Date(thedob.setMinutes(thedob.getMinutes()-thedob.getTimezoneOffset()))
+      .toISOString().slice(0,10);
   }
   cancel()
   {
