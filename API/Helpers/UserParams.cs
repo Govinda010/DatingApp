7fad8d1519b99a3921a0af993417296e5d957375
Govinda010 <gsth010@gmail.com>
2023-06-09 00:47:23 +0200

    pagination for client implemented

--- API/Helpers/UserParams.cs
+++ API/Helpers/UserParams.cs
@@ -4,7 +4,7 @@ namespace API.Helpers;
 public class UserParams
 {
     private const int MaxPageSize = 50;
-    public int PageNumber = 1;
+    public int PageNumber { get; set; } = 1;
     private int _pageSize = 10;
     public int PageSize
     {
--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Member } from 'src/app/_models/member';
+import { Pagination } from 'src/app/_models/pagination';
 import { MembersService } from 'src/app/_services/members.service';
 
 @Component({
@@ -9,13 +10,32 @@ import { MembersService } from 'src/app/_services/members.service';
   styleUrls: ['./member-list.component.css']
 })
 export class MemberListComponent implements OnInit {
-  members$:Observable<Member[]>|undefined;
+  // members$:Observable<Member[]>|undefined;
+  pagination: Pagination | undefined;
+  members: Member[] = [];
+  pageNumber = 1;
+  pageSize = 10;
+
   constructor(private memberSerive:MembersService){}
 
   ngOnInit(): void {
-    this.members$ = this.memberSerive.getMembers();
+    this.loadMembers();
   }
 
+  loadMembers()
+  {
+    this.memberSerive.getMembers(this.pageNumber, this.pageSize).subscribe(
+      {
+        next:response=>{
+          if(response.result && response.pagination)
+          {
+            this.members = response.result;
+            this.pagination = response.pagination;
+          }
+        }
+      }
+    )
+  }
   
 }
 
