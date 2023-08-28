6e77b9ea642826c8dd5e11df5156da9cdd9bffa9
Govinda010 <gsth010@gmail.com>
2023-08-28 04:49:48 +0200

    like functionality is added

--- API/Controllers/LikesController.cs
+++ API/Controllers/LikesController.cs
@@ -1,5 +1,6 @@
 using API.DTOs;
 using API.Extensions;
+using API.Helpers;
 using API.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
@@ -39,9 +40,12 @@ public class LikesController:BaseApiController
     }
 
     [HttpGet]
-    public async Task<ActionResult<IEnumerable<LikeDto>>>GetUser(string predicate)
+    public async Task<ActionResult<PagedList<LikeDto>>>GetUser([FromQuery]LikesParams likesParams)
     {
-        var users = await _likesRepository.GetUsersLikes(predicate, User.GetUserId());
+        likesParams.UserId = User.GetUserId();
+        var users = await _likesRepository.GetUserLikes(likesParams);
+        Response.AddPaginationHeader(new PaginationHeader(users.CurrentPage,
+            users.PageSize, users.TotalCount, users.TotalPages));
         return Ok(users);
 
     }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AccountService } from '../_services/account.service';
 import { Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
+import { MembersService } from '../_services/members.service';
 
 @Component({
   selector: 'app-nav',
@@ -21,12 +22,16 @@ export class NavComponent implements OnInit {
 
   login(){
     this.accountService.login(this.model).subscribe({
-      next:_=>this.router.navigateByUrl('/members')      
+      next:_=>{
+        this.router.navigateByUrl('/members') 
+      }     
     })  
   }
   logout(){
     this.accountService.logout();
     this.router.navigateByUrl('/');
+    
+    //this.memberService.resetUserParams();
   }
 
 }
