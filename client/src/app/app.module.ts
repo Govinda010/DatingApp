fad9ea094a0e61b5ba47fc0e79942150b9e37bcc
Govinda010 <gsth010@gmail.com>
2023-09-03 23:10:06 +0200

    Admin panel is added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -25,6 +25,10 @@ import { PhotoEditorComponent } from './members/photo-editor/photo-editor.compon
 import { TextInputComponent } from './_forms/text-input/text-input.component';
 import { DatePickerComponent } from './_forms/date-picker/date-picker.component';
 import { MemberMessagesComponent } from './members/member-messages/member-messages.component';
+import { AdminPanelComponent } from './admin/admin-panel/admin-panel.component';
+import { HasRoleDirective } from './_directives/has-role.directive';
+import { PhotoManagementComponent } from './admin/photo-management/photo-management.component';
+import { UserManagementComponent } from './admin/user-management/user-management.component';
 
 @NgModule({
   declarations: [
@@ -45,6 +49,10 @@ import { MemberMessagesComponent } from './members/member-messages/member-messag
     TextInputComponent,
     DatePickerComponent,
     MemberMessagesComponent,
+    AdminPanelComponent,
+    HasRoleDirective,
+    UserManagementComponent,
+    PhotoManagementComponent,
   ],
   imports: [
     BrowserModule,
